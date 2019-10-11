836b3bffb251e4544ab877c066964c9bbbee090a
JonatanRek <gamerclassn7@gmail.com>
2019-10-11 17:04:29 +0200

    Netherlands translation addet

---
+++ app/lang/nl.php
@@ -0,0 +1,102 @@
+<?php
+return $lang = [
+	//Menu
+	'm_home' => 'Home',
+	'm_dashboard' => 'Controlepaneel',
+	'm_settings' => 'Instellingen',
+	'm_automatization' => 'Automatisatie',
+	'm_scenes' => 'Scénes',
+	'm_log' => 'Log',
+
+	//Buttons
+	'b_year' => 'Jaar',
+	'b_month' => 'Maand',
+	'b_week' => 'Week',
+	'b_day' => 'Dag',
+	'b_hour' => 'Uur',
+	'b_next' => 'Volgende',
+	'b_create' => 'Maak',
+	'b_edit' => 'Bewerk',
+	'b_remove' => 'Verwijder',
+	'b_approve' => 'Keur goed',
+	'b_disable' => 'Zet uit',
+	'b_save' => 'Opslaan',
+	'b_logOut' => 'Log Uit',
+	'b_sendTestNotification' => 'Verzend Test Notificatie',
+	'b_rooms' => 'Kamers',
+	'b_restart' => 'Herstart',
+	'b_disable' => 'Zet uit',
+	'b_select' => 'Selecteer',
+
+
+	//labels
+	'l_choseDevice' => 'Kies apparaat:',
+	'l_inHome' => 'Bij het inkomen',
+	'l_outHome' => 'Bij het buitengaan',
+	'l_sunSet' => 'Zonsondergang',
+	'l_sunRice' => 'Zonsopkomst',
+	'l_time' => 'Tijd',
+	'l_deviceValue' => 'Apparaat Waarde',
+	'l_runAt' => 'Voer uit bij',
+	'l_resetAt' => 'Reset bij',
+	'l_affectedDevices' => 'Getroffen apparaten',
+	'l_atDays' => 'Op dagen ',
+	'l_read' => 'Lees',
+	'l_use' => 'Gebruik',
+	'l_edit' => 'Bewerk',
+	'l_owner' => 'eigenaar',
+	'l_member' => 'Huis lid',
+	'l_permission' => 'Permissie',
+	'l_inMinutes' => 'in minuten',
+	'l_sleepTime' => 'Apparaat slaaptijd',
+	'l_atHome' => 'Thuis',
+	'l_nameAt' => 'Naam',
+	'l_lastSeen' => 'Laatst gezien',
+	'l_notificationStatus' => 'Notificatie status',
+
+	//Title
+	't_createScene' => 'Maak scéne',
+	't_editScene' => 'Bewerk scéne',
+	't_createAutomation' => 'Maak automatisatie',
+	't_addDevice' => 'Voeg apparaat toe',
+	't_editDevice' => 'Bewerk apparaat',
+	't_pageAfterLogIn' => 'Pagina na Login',
+	't_profile' => 'Profiel',
+	't_notification' => 'Notificatie',
+	't_experimental' => 'experimenteel',
+
+	//constants
+	'humi' => 'Vochtigheid',
+	'temp' => 'Temperatuur',
+	'light' => 'Licht',
+	'battery' => 'Batterij',
+	'on/off' => 'Schakelaar',
+
+	//words
+	'w_title' => 'Naam',
+	'w_icon' => 'Icoon',
+	'w_no' => 'nee',
+	'w_noOne' => 'niemand',
+	'w_someOne' => 'Sommige',
+	'w_room' => 'Kamer',
+	'w_moduls' => 'Modules',
+	'w_home' => 'Huis',
+	'w_neni' => 'Bij',
+	'w_is' => 'is',
+
+	//tables
+	't_time' => 'Tijd',
+	't_state' => 'Status',
+
+	//Days Long
+	'd_monday' => 'Maandag',
+	'd_tuesday' => 'Dinsdag',
+	'd_wednesday' => 'Woensdag',
+	'd_thursday' => 'Donderdag',
+	'd_friday' => 'Vrijdag',
+	'd_saturday' => 'Zaterdag',
+	'd_sunday' => 'Zondag',
+
+	//example
+	'' => '',
+];
