7998c9eedc77d7c31c4919b2448d8408173d03db
JonatanRek <gamerclassn7@gmail.com>
2019-10-13 14:00:41 +0200

    User Creation #2

--- app/controls/setting.php
+++ app/controls/setting.php
@@ -5,6 +5,13 @@ if (isset($_POST) && !empty($_POST)){
 		$newPassword = $_POST['newPassword1'];
 		$newPassword2 = $_POST['newPassword2'];
 		UserManager::changePassword($oldPassword, $newPassword, $newPassword2);
-		//TODO: pridat odhlášení
+		header('Location: ' . BASEDIR . 'logout');
+		die();
+	} else if (isset($_POST['submitCreateUser']) && $_POST['submitCreateUser'] != "") {
+		$userName = $_POST['userName'];
+		$password = $_POST['userPassword'];
+		UserManager::createUser($userName, $password);
+		header('Location: ' . BASEDIR . 'setting');
+		die();
 	}
 }
