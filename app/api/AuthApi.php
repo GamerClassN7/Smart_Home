d137d08ff4d0acbc23d30c225862a50b86d1877c
xinatorus <patrik.sztefek@seznam.cz>
2020-04-27 19:42:05 +0200

    AuthApi fix, refactoring

--- app/api/AuthApi.php
+++ app/api/AuthApi.php
@@ -1,25 +1,25 @@
 <?php
 class AuthApi {
-    static function login(){
-        $token = (new ApiManager)->getToken($this->input->username,$this->input->password);
-        if (!$token) {
-            throw new Exception("Auth failed", 401);
-        }
-        $this->response(['token' => $token]);
-    }
+	public function login(){
+		$token = (new ApiManager)->getToken($this->input->username,$this->input->password);
+		if (!$token) {
+			throw new Exception("Auth failed", 401);
+		}
+		$this->response(['token' => $token]);
+	}
 
-    static function logout(){
-        $authenticationBearrer = $_SERVER['HTTP_AUTHORIZATION'];
-        if (!(new ApiManager)->deleteToken($authenticationBearrer)) {
-            throw new Exception("logout Failed", 401);
-        }
-    }
+	public function logout(){
+		$authenticationBearrer = $_SERVER['HTTP_AUTHORIZATION'];
+		if (!(new ApiManager)->deleteToken($authenticationBearrer)) {
+			throw new Exception("logout Failed", 401);
+		}
+	}
 
-    static function registration(){
-        
-    }
+	public function registration(){
 
-    static function restartPassword(){
-        
-    }
-} 
+	}
+
+	public function restartPassword(){
+
+	}
+}
