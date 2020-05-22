0929870cc9bc0871f1f3556503966ac57536980a
xinatorus <patrik.sztefek@seznam.cz>
2020-05-22 20:11:52 +0200

    api response content header as json

--- library/ApiController.php
+++ library/ApiController.php
@@ -30,6 +30,7 @@ class ApiController {
 	}
 
 	protected function response($data = [], $httpCode = '200'){
+		header('Content-Type: application/json');
 		http_response_code($httpCode);
 		echo json_encode($data, JSON_UNESCAPED_UNICODE);
 	}
