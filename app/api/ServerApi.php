49d24f8e0e4eda030118e825faa3fb346d12c022
JonatanRek <gamerclassn7@gmail.com>
2020-07-27 19:52:43 +0200

    Server Stats API

---
+++ app/api/ServerApi.php
@@ -0,0 +1,28 @@
+<?php
+class ServerApi extends ApiController {
+    private function getSystemMemInfo()
+	{
+		$data = explode("\n", file_get_contents("/proc/meminfo"));
+		$meminfo = array();
+		foreach ($data as $line) {
+			$data = explode(":", $line);
+			if (count($data)!=2) continue;
+			$meminfo[$data[0]] = trim($data[1]);
+		}
+		return $meminfo;
+    }
+
+    public function default(){
+        //$this->requireAuth();
+        $response = [
+            "cpu_load" => sys_getloadavg()[0],
+            "uptime" => shell_exec('uptime -p'),
+            "ramFree" => $this->getSystemMemInfo()["MemFree"],
+            "ramTotal" => $this->getSystemMemInfo()["MemTotal"],
+            "diskFree" => disk_free_space("/"),
+            "diskTotal" => disk_total_space("/"),
+            "serverTime" =>  date('m. d. Y H:i:s - e'),
+        ];
+        $this->response($response);
+    }
+}
