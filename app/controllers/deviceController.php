2c6e7e2eccbeea0537812372231836d3552182de
Haitem <silentislp@gmail.com>
2020-09-08 18:00:44 +0200

    Add function on change room

--- app/controllers/deviceController.php
+++ app/controllers/deviceController.php
@@ -15,11 +15,14 @@ if (!empty ($_POST)){
 	}
 	if (isset ($_POST['deviceCommand'])  && !empty ($_POST['deviceId'])) {
 		$deviceManager->edit ($_POST['deviceId'], array ('command' => $_POST['deviceCommand']));
-	}else if (!empty ($_POST['deviceCommand'])) {
+	} else if (!empty ($_POST['deviceCommand'])) {
 		$devices = $deviceManager->getAllDevices();
 		foreach ($devices as $key => $device) {
 			$deviceManager->edit ($device['device_id'], array ('command' => $_POST['deviceCommand']));
 		}
+	}
+	if (!empty ($_POST['deviceRoomId'])  && !empty ($_POST['deviceId'])) {
+		$deviceManager->edit ($_POST['deviceId'], array ('room_id' => $_POST['deviceRoomId']));
 	}
 	header('Location: ./device');
 	die();
