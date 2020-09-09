cc804f36dfa2ad55371e0c3dc48c661bb2a076b7
Haitem <silentislp@gmail.com>
2020-09-09 20:24:10 +0200

    Create sorting on table in devices

--- app/views/Device.php
+++ app/views/Device.php
@@ -16,7 +16,61 @@ class Device extends Template
 		$template = new Template ('device');
 		$template->prepare ('title', $langMng->get ("m_devices"));
 
-		$devices = $deviceManager->getAllDevices ();
+		if (!empty ($_GET['sort']) && !empty ($_SESSION['sort']) && $_SESSION['sort'] != $_GET['sort']) {
+			unset($_SESSION['sort']);
+			header('Location: device?sort=' . $_GET["sort"] . "&sortType=ASC");
+			die();
+		}
+
+		if (isset ($_GET['sortType'])) {
+			switch ($_GET['sortType']) {
+				case "DESC":
+				$sortType = "";
+				$sortIcon = "&#xf0dd";
+				break;
+				case "ASC":
+				$sortType = "DESC";
+				$sortIcon = "&#xf0de";
+				break;
+				case "":
+				unset($_GET["sort"]);
+				unset($_GET["sortType"]);
+				header('Location: device');
+				die();
+				break;
+			}
+		} else {
+			$sortType = "ASC";
+		}
+
+		if (!empty ($_GET['sort']) && !empty ($_GET['sortType'])) {
+			$template->prepare ('sortIcon', array ($_GET['sort'] => $sortIcon));
+			$actualSort = "devices.device_id";
+			switch ($_GET['sort']) {
+				case "name":
+				$actualSort = "devices.name";
+				break;
+				case "room":
+				$actualSort = "rooms.name";
+				break;
+				case "ip":
+				$actualSort = "devices.ip_address";
+				break;
+				case "mac":
+				$actualSort = "devices.mac";
+				break;
+				case "token":
+				$actualSort = "devices.token";
+				break;
+			}
+			$devices = $deviceManager->getAllDevicesSorted ($actualSort, $_GET['sortType']);
+		} else {
+			$devices = $deviceManager->getAllDevices ();
+		}
+
+		if (!empty ($_GET['sort'])) {
+			$_SESSION['sort'] = $_GET['sort'];
+		}
 
 		foreach ($devices as $key => $device) {
 			$localBinary = "../updater/" . str_replace (':', '', $device['mac']) . ".bin";
@@ -30,6 +84,21 @@ class Device extends Template
 			} else {
 				$devices[$key]['firmware_hash'] = "false";
 			}
+			if (empty ($device['mac'])) {
+				$devices[$key]['firmware_hash'] = "";
+			}
+		}
+
+		if (!empty ($_GET['sort']) && !empty ($_GET['sortType']) && $_GET['sort'] == "firmware") {
+			if ($_GET['sortType'] == "DESC") {
+				usort($devices, function($a, $b) {
+					return $a['firmware_hash'] <=> $b['firmware_hash'];
+				});
+			} else if ($_GET['sortType'] == "ASC") {
+				usort($devices, function($a, $b) {
+					return $b['firmware_hash'] <=> $a['firmware_hash'];
+				});
+			}
 		}
 
 		$rooms = $roomManager->getAllRooms();
@@ -38,6 +107,7 @@ class Device extends Template
 		$template->prepare ('debugMod', DEBUGMOD);
 		$template->prepare ('logToLiveTime', LOGTIMOUT);
 		$template->prepare ('rooms', $rooms);
+		$template->prepare ('sortType', $sortType);
 		$template->prepare ('devices', $devices);
 		$template->prepare ('langMng', $langMng);
 
