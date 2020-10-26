eba00c2744a58d1fbfec3841d72e6fdff6be49f0
Haitem <silentislp@gmail.com>
2020-10-26 16:45:45 +0100

    Remake design for plugins buttons

--- app/controllers/pluginsController.php
+++ app/controllers/pluginsController.php
@@ -2,13 +2,16 @@
 if (!empty ($_POST)){
 	if (
 		isset($_POST['name']) &&
-		$_POST['name'] != '' &&
-		isset($_POST['actualStatus'])
+		$_POST['name'] != ''
 	){
-		if ($_POST['actualStatus']) {
-			rename($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/' . $_POST['name'] . ".php", $_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/!' . $_POST['name'] . ".php");
+		if ($_POST['status'] == "true") {
+			if (file_exists ($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/!' . $_POST['name'] . ".php")) {
+				rename($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/!' . $_POST['name'] . ".php", $_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/' . $_POST['name'] . ".php");
+			}
 		} else {
-			rename($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/!' . $_POST['name'] . ".php", $_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/' . $_POST['name'] . ".php");
+			if (file_exists ($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/' . $_POST['name'] . ".php")) {
+				rename($_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/' . $_POST['name'] . ".php", $_SERVER['DOCUMENT_ROOT'] . BASEDIR . 'app/plugins/!' . $_POST['name'] . ".php");
+			}
 		}
 		header('Location: ./plugins');
 		die();
