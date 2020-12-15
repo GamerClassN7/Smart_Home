28dd69e3a5f1e9a69af9c624c7fa917dfaba2ffd
Romano Schoonheim <romano@romanoschoonheim.nl>
2020-12-15 21:55:39 +0100

    Write concerns

--- core/Configuration/Configurations.php
+++ core/Configuration/Configurations.php
@@ -8,4 +8,14 @@ namespace Core\Configuration;
  * @author Romano Schoonheim https://github.com/romano1996
  */
 class Configurations
-{}
+{
+	/**
+	 * Concerns:
+	 * 	-> Loading configuration files
+	 * 		- Scan directory for files.
+	 * 		- Filtering none config / php files.
+	 * 		- Creating assoc array.
+	 * 	-> Storing assoc array to this object.
+	 * 	-> Accessing configurations based on "paths" application.something For example.
+	 */
+}
