cfcfefefd898dc42d120e8f752696a0ed85e0829
Romano Schoonheim <romano@romanoschoonheim.nl>
2020-12-15 16:59:08 +0100

    Bootstrap installed

--- public/index.php
+++ public/index.php
@@ -4,3 +4,8 @@
  * Composer autoload
  */
 	require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * todo: Create a new bootstrap architecture.
+ */
+require_once __DIR__ . '/../app/Bootstrap.php';
