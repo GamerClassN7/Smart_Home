dc77534bad6ed76e86d6b47d11d39ed345949c63
JonatanRek <vasek@steelants.cz>
2021-01-07 18:02:01 +0100

    Weather notifications

--- app/plugins/OpenWeatherMap.php
+++ app/plugins/OpenWeatherMap.php
@@ -23,6 +23,38 @@ class OpenWeatherMap extends VirtualDeviceManager
 				$finalUrl = sprintf($this->api_uri, $this->city_sluig, $this->app_id);
 				$json = json_decode(Utilities::CallAPI('GET', $finalUrl, ''), true);
 
+
+				//Notification data setup
+				$notificationMng = new NotificationManager;
+				if ($json['weather'][0]['id'] >= 500 && $json['weather'][0]['id'] < 600) {
+					// $notificationData = [
+					// 	'title' => 'Weather',
+					// 	'body' => 'It Will be rainy outhere, Take Umbrela :)',
+					// 	'icon' => 'http://dev.steelants.cz/projekty/simplehome-client/img/icons/favicon-16x16.png',
+					// ];
+					// //Notification for newly added Device
+					// if ($notificationData != []) {
+					// 	$subscribers = $notificationMng::getSubscription();
+					// 	foreach ($subscribers as $key => $subscriber) {
+					// 		$notificationMng::sendSimpleNotification(SERVERKEY, $subscriber['token'], $notificationData);
+					// 	}
+					// }
+				} else if ($json['weather'][0]['id'] >= 600 && $json['weather'][0]['id'] < 700) {
+					// $notificationData = [
+					// 	'title' => 'Weather',
+					// 	'body' => 'It is white out there :)',
+					// 	'icon' => 'http://dev.steelants.cz/projekty/simplehome-client/img/icons/favicon-16x16.png',
+					// ];
+					// //Notification for newly added Device
+					// if ($notificationData != []) {
+					// 	$subscribers = $notificationMng::getSubscription();
+					// 	foreach ($subscribers as $key => $subscriber) {
+					// 		$notificationMng::sendSimpleNotification(SERVERKEY, $subscriber['token'], $notificationData);
+					// 	}
+					// }
+				}
+
+
 				RecordManager::create($deviceId, $this->subdevice_type, $json['weather'][0]['id']);
 			} else {
 				DeviceManager::create($this->virtual_device_name, $this->virtual_device_name, 'senzore-virtual');
